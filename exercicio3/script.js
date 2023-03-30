2466aeb39d7f928a421d2a5c5845bb4d22f1bc4b
anabfer <abf.ferreirac@gmail.com>
2023-03-30 15:49:13 -0300

    Exercícios de Laços II - Feito

---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let ranking = 1
+
+for(let paises of maioresPaises){
+
+    console.log('Posição', ranking, ':' , paises)
+    ranking++
+    
+}
